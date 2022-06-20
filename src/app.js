98193b1a2d26f17ee906e79856180171341162db
Felix developer <felixxdeveloper@gmail.com>
2022-06-20 13:14:34 -0300

    Added http2 module at project

--- src/app.js
+++ src/app.js
@@ -1,27 +1,35 @@
-const http = require('http');
-const { formatUrl } = require('./utils');
+const http2 = require('http2');
+const fs = require('fs');
 const routes = require('./routes');
+const { formatUrl } = require('./utils');
 const { DEFAULT_HEADER } = require('./config');
 
-const server = http.createServer(async (request, response) => {
-  try {
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, DELETE, OPTIONS"
-    );
+const sslSettings = {
+  key: fs.readFileSync('localhost-privkey.pem'),
+  cert: fs.readFileSync('localhost-cert.pem')
+}
 
-    const { url, method } = request;
+const secureServer = http2.createSecureServer(sslSettings)
+  .on('request', async (request, response) => {
+    try {
+      response.setHeader("Access-Control-Allow-Origin", "*");
+      response.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS"
+      );
 
-    const urlFormatted = formatUrl(url, method);
+      const { url, method } = request;
 
-    const route = routes[urlFormatted] || routes.default;
+      const urlFormatted = formatUrl(url, method);
 
-    route(request, response);
-    
-  } catch (error) {
-    response.writeHead(500, DEFAULT_HEADER).end();
-  }
-});
+      const route = routes[urlFormatted] || routes.default;
 
-module.exports = server;
+      await route(request, response);
+
+    } catch (error) {
+      response.writeHead(500, DEFAULT_HEADER).end();
+    }
+  })
+  .on('error', (error) => console.log(error))
+
+module.exports = secureServer;
