a0b726237d0f51feadda8ec0097671e95c443825
Felix developer <felixxdeveloper@gmail.com>
2022-08-05 11:32:35 -0300

    REF - Better create user ednpoint

--- src/useCases/users/create/createUser.js
+++ src/useCases/users/create/createUser.js
@@ -1,10 +1,15 @@
-const { JDB } = require('../../../JDB');
+const { JDB } = require('../../../JDB/index');
 
 async function createUser(user) {
   try {
-    const Users = new JDB('users');
-    
-    await Users.set(user);
+    const Users = new JDB({ model: 'users' });
+
+    const isInvalid = Users.isInvalid(user);
+
+    if (isInvalid)
+      return isInvalid;
+
+    await Users.create(user);
 
   } catch (error) {
     return new Error('Error creating user');
--- src/useCases/users/create/createUserController.js
+++ src/useCases/users/create/createUserController.js
@@ -1,23 +1,29 @@
+const { Request, Response } = require('../../../config/RequestMethods');
 const { randomUUID: uuid } = require('crypto');
 const { createUser } = require('./createUser');
-const { Request, Response } = require('../../../config/RequestMethods');
+
+const formatMessage = (messages = ['']) =>
+  messages.map((message) => message + ' is invalid').join(', ');
 
 async function createUserController(request = Request, response = Response) {
   try {
     const { username } = request.body;
 
     if (!username)
-      throw new Error()
+      throw new Error();
 
     const user = {
       _id: uuid(),
-      username: username.toLowerCase()
+      username: username?.toLowerCase?.()
     }
 
     const result = await createUser(user);
 
     if (result instanceof Error)
       return response.status(400).json(result.message);
+
+    if (Array.isArray(result))
+      return response.status(400).json(formatMessage(result));
 
     return response.status(201).json(user);
 
